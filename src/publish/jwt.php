06490606d7b368679e27252f7e9aae6e68768bcf
japool <jappol@163.com>
2024-08-06 02:06:00 +0800

    添加xlswrite 类库

--- src/publish/jwt.php
+++ src/publish/jwt.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 return [
-    'secret' => \Hyperf\Support\env('JWT_SECRET', 3600000),
+    'secret' => \Hyperf\Support\env('JWT_SECRET', 'azqwrzbie3d5d0061126d0ca0320daf761444bdbe52ba4fac580932ce0ddc9ad'),
     'algorithm' => 'HS256',
     'exp' => \Hyperf\Support\env('JWT_TOKEN_TIME_OUT',3600),
     'exclude' => [
