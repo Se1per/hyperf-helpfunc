8b9de6f866381d9956adeda97bc9fc56fd46b6c5
japool@163.com <japool@163.com>
2024-08-12 15:05:45 +0800

    添加身份证类和修复xls 修复部分json 静态

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -34,7 +34,7 @@ class ConfigProvider
                     'id' => 'middleware',
                     'description' => 'JwtTokenMiddleware generate', // 描述
                     // 建议默认配置放在 publish 文件夹中，文件命名和组件名称相同
-                    'source' => __DIR__ . '/publish/JwtTokenMiddleware.php.copy',  // 对应的配置文件路径
+                    'source' => __DIR__ . '/publish/JwtTokenMiddleware.stub',  // 对应的配置文件路径
                     'destination' => BASE_PATH . '/app/Middleware/JwtTokenMiddleware.php', // 复制为这个路径下的该文件
                 ],
             ],
